e38dd779fb463665d949543881412202a8fe8252
Lingye Zhuang <l1zhuang@ucsd.edu>
2021-01-21 03:14:56 -0800

    added Javescript

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,14 @@
  */
 
 exports.view = function(req, res){
+  // request parameter, if doesn't exist, set default to "World"
+  var nameToShow = req.params.userName;
+  if (nameToShow === undefined) {
+    nameToShow = "World";
+  }
+  // debug statement
+  console.log ("Name is: " + nameToShow);
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
